cd44939491fd388e969d70f8df7edad131be18a0
Hairui <hairui@sorealprop.com>
2018-09-10 11:20:32 +0800

    Initial Commit

---
+++ UIViewPropertyAnimatorTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIViewPropertyAnimatorTest
+//
+//  Created by Hairui on 10/9/18.
+//  Copyright © 2018 Hairui's Organisation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
